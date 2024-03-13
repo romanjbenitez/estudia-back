981fe806882b7fcb9fbf001df4430026e9dcca00
romanjbenitez <roman200217@gmail.com>
2024-03-13 09:56:52 -0300

    Add user router

---
+++ routers/users.py
@@ -0,0 +1,77 @@
+# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=5382
+
+### Users API ###
+
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
+
+router = APIRouter()
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    anwers : list[str]
+
+
+users_list = [User(id=1,username="roman", anwers=["es verdad que una paloma puede volar a 30km/h" , "Si es verdad"]),]
+
+
+
+@router.get("/users")
+async def users():
+    return users_list
+
+
+@router.get("/user/{id}")  # Path
+async def user(id: int):
+    return search_user(id)
+
+
+
+@router.post("/user/", response_model=User, status_code=201)
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        raise HTTPException(status_code=404, detail="El usuario ya existe")
+
+    users_list.append(user)
+    return user
+
+
+@router.put("/user/")
+async def user(user: User):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if not found:
+        return {"error": "No se ha actualizado el usuario"}
+
+    return user
+
+
+@router.delete("/user/{id}")
+async def user(id: int):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+
+    if not found:
+        return {"error": "No se ha eliminado el usuario"}
+
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
